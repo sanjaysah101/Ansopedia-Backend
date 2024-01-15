610bc9e7e35a02bb65c96bbfff0cfb872a0fea13
Sanjay Kumar Sah <90146483+sanjaysah101@users.noreply.github.com>
2024-01-15 22:54:48 +0530

    Update Firebase.js

--- config/Firebase.js
+++ config/Firebase.js
@@ -1,12 +1,7 @@
 const {initializeApp} = require("firebase/app");
 // const credentials = require("./firebase-adminsdk.json");
 const firebaseConfig = {
-    apiKey: "AIzaSyDC50dtiCOHa8iav2te31Z5UNb7e2gi0uo",
-    authDomain: "ansopedia-aeb06.firebaseapp.com",
-    projectId: "ansopedia-aeb06",
-    storageBucket: "ansopedia-aeb06.appspot.com",
-    messagingSenderId: "290159073203",
-    appId: "1:290159073203:web:92b79c2339d34f854b9dc6"
+
   };
 
  
